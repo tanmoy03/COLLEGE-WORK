969134a4361d74eed0d825766d91c2918fd67cd7
tanmoy03 <tanmoydebnath2003@gmail.com>
2021-10-18 20:20:59 +0100

    firstupload

---
+++ PROGRAMMING/CS1117/testing1.py
@@ -0,0 +1,11 @@
+def ask_for_name(name1 , name2):
+    fullname = input(name1)
+    fulllname = input(name2)
+    fulllname.lower()
+    fullname.lower()
+    print(fulllname,fulllname)
+    
+
+name1 = "What is your first name "
+name2 = "What is your surname "
+ask_for_name(name1 , name2)
