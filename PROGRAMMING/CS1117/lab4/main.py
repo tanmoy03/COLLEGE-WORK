010b4577290f3a5448e93c6d7827ad81582043e8
tanmoy03 <tanmoydebnath2003@gmail.com>
2021-10-26 16:20:45 +0100

    firstupload

---
+++ PROGRAMMING/CS1117/lab4/main.py
@@ -0,0 +1,22 @@
+# ScriptName: main.py
+# Author: Tanmoy Debnath 121324683
+
+# template for calling functions in another file
+
+# import my_functions from other files - different options
+# from my_functions import print_function
+# import my_functions - when you use this you need to call the function using 'my_functions.<function_name>'
+# this option imports all my_functions, using '*'
+from my_functions import *
+
+
+def main():
+    """
+    Call the functions defined in the functions.py file
+    """
+    print_function()
+
+
+if __name__ == "__main__":
+    ''' call the main() function in this file '''
+    main()
---
+++ PROGRAMMING/CS1117/lab4/my_functions.py
@@ -0,0 +1,8 @@
+# ScriptName: my_functions.py
+# Author: <add your name here>
+
+# template for calling functions in another file
+
+
+def print_function():
+    print("I'm in another file :)")
